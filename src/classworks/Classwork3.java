f5bd1a2d83e13e4447d81381fafe6d016260ca50
Artem <twistersister987@gmail.com>
2024-11-17 16:52:56 +0300

    change

--- src/classworks/Classwork3.java
+++ src/classworks/Classwork3.java
@@ -6,6 +6,7 @@ import java.util.Scanner;
 public class Classwork3 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        System.out.println("Внесены изменения");
 
 
         // 2
